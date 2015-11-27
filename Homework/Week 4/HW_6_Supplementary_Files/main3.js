1c9e2d3791e4c2355820f4a39029fddca8aa89c8
BartQuaink <bartquaink@live.nl>
2015-11-27 21:09:59 +0100

    tussencommit

--- Homework/Week 4/HW_6_Supplementary_Files/main3.js
+++ Homework/Week 4/HW_6_Supplementary_Files/main3.js
@@ -1,13 +1,346 @@
+colours = ["#f90606", "#fc4e03", "#fe9903", "#fac404", "#faf705", "#e0fb04"];
+
+// arbitrary legend sizes
+var legend1 = 2000000;
+var legend2 = 5000000;
+var legend3 = 10000000;
+var legend4 = 15000000;
+var legend5 = 30000000;
+var legend6 = 50000000;
+
 /* use this to test out your function */
 window.onload = function() {
-  changeColor("de-2", "#c81616");
-  changeColor("lt-3", "#16d646");
-  changeColor("si-9", "#2993c2");
-    changeColor("is-4", "#c2ee0f");
+
+  // get wanted data
+  var data = document.getElementById("rawdata").innerHTML;
+  data = JSON.parse(data);
+
+  // create lists to store country and population
+  var country = [];
+  var population = [];
+
+  // iterate over whole json list
+  for (var i = 0; i < data.length; i++) {
+    population.push(data[i][3]);
+
+    var a = 0;
+    // push country codes in list
+    while (a < country_codes.length) {
+      if (data[i][1] === country_codes[a][2]){
+        country.push(country_codes[a][0]);
+        a = 0;
+        break;
+      }
+      else {
+      a++;
+      }
+    }
+  }
+
+  var sortedcountries = [];
+  var countrynumbers = 0;
+
+  // create list containing all the african countries' iso alpha-2 codes.
+  var africountriesiso = ["dz", "ao", "sh", "bj", "bw", "bf", "bi", "cm", "cv", "cf", "td", "km", "cg", "dj", "eg", "gq", "er", "et", "ga", "gm", "gh", "gw", "gn", "ci", "ke", "ls", "lr", "ly", "mg", "mw", "ml", "mr", "mu", "yt", "ma", "mz", "na", "ne", "ng", "st", "re", "rw", "sn", "sc", "sl", "so", "za", "sh", "sd", "sz", "tz", "tg", "tn", "ug", "cd", "zm", "zw", "ss"];
+
+  // create list for population
+  var africountriespop = [];
+
+  // create vars
+  var colored = africountriesiso.length;
+  var j = 0;
+
+  // create list with african population corresponding with africountriesiso, this is where the script gets stuck, no idea why
+  while (j <= colored - 1) {
+    var k = 0;
+    if (africountriesiso[j] === country[k]) {
+      africountriespop[j].push(population[k]);
+      j++;
+      k = 0;
+    }
+    else {
+      k++;
+    }
+  }
+
+  // time to color all the countries
+  for (l = 0; l < colored; l++) {
+    if (population[l] < legend1) {
+      changeColor(africountriesiso[l], colours[0]);
+    }
+    if (legend1 < population[l] < legend2) {
+      changeColor(africountriesiso[l], colours[1]);
+    }
+    if (legend2 < population[l] < legend3) {
+      changeColor(africountriesiso[l], colours[2]);
+    }
+    if (legend3 < population[l] < legend4) {
+      changeColor(africountriesiso[l], colours[3]);
+    }
+    if (legend4 < population[l] < legend5) {
+      changeColor(africountriesiso[l], colours[4]);
+    }
+    else {
+      changeColor(africountriesiso[l], colours[5]);
+    }
+  }
 };
 
 /* changeColor takes a path ID and a color (hex value)
    and changes that path's fill color */
-function changeColor(id, color) {
+function changeColor(id, color){
   document.getElementById(id).style.fill = color;
 }
+
+var country_codes = [
+    ["af", "AFG", "Afghanistan"],
+    ["ax", "ALA", "Åland Islands"],
+    ["al", "ALB", "Albania"],
+    ["dz", "DZA", "Algeria"],
+    ["as", "ASM", "American Samoa"],
+    ["ad", "AND", "Andorra"],
+    ["ao", "AGO", "Angola"],
+    ["ai", "AIA", "Anguilla"],
+    ["aq", "ATA", "Antarctica"],
+    ["ag", "ATG", "Antigua and Barbuda"],
+    ["ar", "ARG", "Argentina"],
+    ["am", "ARM", "Armenia"],
+    ["aw", "ABW", "Aruba"],
+    ["au", "AUS", "Australia"],
+    ["at", "AUT", "Austria"],
+    ["az", "AZE", "Azerbaijan"],
+    ["bs", "BHS", "Bahamas"],
+    ["bh", "BHR", "Bahrain"],
+    ["bd", "BGD", "Bangladesh"],
+    ["bb", "BRB", "Barbados"],
+    ["by", "BLR", "Belarus"],
+    ["be", "BEL", "Belgium"],
+    ["bz", "BLZ", "Belize"],
+    ["bj", "BEN", "Benin"],
+    ["bm", "BMU", "Bermuda"],
+    ["bt", "BTN", "Bhutan"],
+    ["bo", "BOL", "Bolivia, Plurinational State of"],
+    ["bq", "BES", "Bonaire, Sint Eustatius and Saba"],
+    ["ba", "BIH", "Bosnia and Herzegovina"],
+    ["bw", "BWA", "Botswana"],
+    ["bv", "BVT", "Bouvet Island"],
+    ["br", "BRA", "Brazil"],
+    ["io", "IOT", "British Indian Ocean Territory"],
+    ["bn", "BRN", "Brunei Darussalam"],
+    ["bg", "BGR", "Bulgaria"],
+    ["bf", "BFA", "Burkina Faso"],
+    ["bi", "BDI", "Burundi"],
+    ["kh", "KHM", "Cambodia"],
+    ["cm", "CMR", "Cameroon"],
+    ["ca", "CAN", "Canada"],
+    ["cv", "CPV", "Cape Verde"],
+    ["ky", "CYM", "Cayman Islands"],
+    ["cf", "CAF", "Central African Republic"],
+    ["td", "TCD", "Chad"],
+    ["cl", "CHL", "Chile"],
+    ["cn", "CHN", "China"],
+    ["cx", "CXR", "Christmas Island"],
+    ["cc", "CCK", "Cocos (Keeling) Islands"],
+    ["co", "COL", "Colombia"],
+    ["km", "COM", "Comoros"],
+    ["cg", "COG", "Congo"],
+    ["cd", "COD", "Congo, the Democratic Republic of the"],
+    ["ck", "COK", "Cook Islands"],
+    ["cr", "CRI", "Costa Rica"],
+    ["ci", "CIV", "Côte d'Ivoire"],
+    ["hr", "HRV", "Croatia"],
+    ["cu", "CUB", "Cuba"],
+    ["cw", "CUW", "Curaçao"],
+    ["cy", "CYP", "Cyprus"],
+    ["cz", "CZE", "Czech Republic"],
+    ["dk", "DNK", "Denmark"],
+    ["dj", "DJI", "Djibouti"],
+    ["dm", "DMA", "Dominica"],
+    ["do", "DOM", "Dominican Republic"],
+    ["ec", "ECU", "Ecuador"],
+    ["eg", "EGY", "Egypt"],
+    ["sv", "SLV", "El Salvador"],
+    ["gq", "GNQ", "Equatorial Guinea"],
+    ["er", "ERI", "Eritrea"],
+    ["ee", "EST", "Estonia"],
+    ["et", "ETH", "Ethiopia"],
+    ["fk", "FLK", "Falkland Islands (Malvinas)"],
+    ["fo", "FRO", "Faroe Islands"],
+    ["fj", "FJI", "Fiji"],
+    ["fi", "FIN", "Finland"],
+    ["fr", "FRA", "France"],
+    ["gf", "GUF", "French Guiana"],
+    ["pf", "PYF", "French Polynesia"],
+    ["tf", "ATF", "French Southern Territories"],
+    ["ga", "GAB", "Gabon"],
+    ["gm", "GMB", "Gambia"],
+    ["ge", "GEO", "Georgia"],
+    ["de", "DEU", "Germany"],
+    ["gh", "GHA", "Ghana"],
+    ["gi", "GIB", "Gibraltar"],
+    ["gr", "GRC", "Greece"],
+    ["gl", "GRL", "Greenland"],
+    ["gd", "GRD", "Grenada"],
+    ["gp", "GLP", "Guadeloupe"],
+    ["gu", "GUM", "Guam"],
+    ["gt", "GTM", "Guatemala"],
+    ["gg", "GGY", "Guernsey"],
+    ["gn", "GIN", "Guinea"],
+    ["gw", "GNB", "Guinea-Bissau"],
+    ["gy", "GUY", "Guyana"],
+    ["ht", "HTI", "Haiti"],
+    ["hm", "HMD", "Heard Island and McDonald Islands"],
+    ["va", "VAT", "Holy See (Vatican City State)"],
+    ["hn", "HND", "Honduras"],
+    ["hk", "HKG", "Hong Kong"],
+    ["hu", "HUN", "Hungary"],
+    ["is", "ISL", "Iceland"],
+    ["in", "IND", "India"],
+    ["id", "IDN", "Indonesia"],
+    ["ir", "IRN", "Iran, Islamic Republic of"],
+    ["iq", "IRQ", "Iraq"],
+    ["ie", "IRL", "Ireland"],
+    ["im", "IMN", "Isle of Man"],
+    ["il", "ISR", "Israel"],
+    ["it", "ITA", "Italy"],
+    ["jm", "JAM", "Jamaica"],
+    ["jp", "JPN", "Japan"],
+    ["je", "JEY", "Jersey"],
+    ["jo", "JOR", "Jordan"],
+    ["kz", "KAZ", "Kazakhstan"],
+    ["ke", "KEN", "Kenya"],
+    ["ki", "KIR", "Kiribati"],
+    ["kp", "PRK", "Korea, Democratic People's Republic of"],
+    ["kr", "KOR", "Korea, Republic of"],
+    ["kw", "KWT", "Kuwait"],
+    ["kg", "KGZ", "Kyrgyzstan"],
+    ["la", "LAO", "Lao People's Democratic Republic"],
+    ["lv", "LVA", "Latvia"],
+    ["lb", "LBN", "Lebanon"],
+    ["ls", "LSO", "Lesotho"],
+    ["lr", "LBR", "Liberia"],
+    ["ly", "LBY", "Libya"],
+    ["li", "LIE", "Liechtenstein"],
+    ["lt", "LTU", "Lithuania"],
+    ["lu", "LUX", "Luxembourg"],
+    ["mo", "MAC", "Macao"],
+    ["mk", "MKD", "Macedonia, the former Yugoslav Republic of"],
+    ["mg", "MDG", "Madagascar"],
+    ["mw", "MWI", "Malawi"],
+    ["my", "MYS", "Malaysia"],
+    ["mv", "MDV", "Maldives"],
+    ["ml", "MLI", "Mali"],
+    ["mt", "MLT", "Malta"],
+    ["mh", "MHL", "Marshall Islands"],
+    ["mq", "MTQ", "Martinique"],
+    ["mr", "MRT", "Mauritania"],
+    ["mu", "MUS", "Mauritius"],
+    ["yt", "MYT", "Mayotte"],
+    ["mx", "MEX", "Mexico"],
+    ["fm", "FSM", "Micronesia, Federated States of"],
+    ["md", "MDA", "Moldova, Republic of"],
+    ["mc", "MCO", "Monaco"],
+    ["mn", "MNG", "Mongolia"],
+    ["me", "MNE", "Montenegro"],
+    ["ms", "MSR", "Montserrat"],
+    ["ma", "MAR", "Morocco"],
+    ["mz", "MOZ", "Mozambique"],
+    ["mm", "MMR", "Myanmar"],
+    ["na", "NAM", "Namibia"],
+    ["nr", "NRU", "Nauru"],
+    ["np", "NPL", "Nepal"],
+    ["nl", "NLD", "Netherlands"],
+    ["nc", "NCL", "New Caledonia"],
+    ["nz", "NZL", "New Zealand"],
+    ["ni", "NIC", "Nicaragua"],
+    ["ne", "NER", "Niger"],
+    ["ng", "NGA", "Nigeria"],
+    ["nu", "NIU", "Niue"],
+    ["nf", "NFK", "Norfolk Island"],
+    ["mp", "MNP", "Northern Mariana Islands"],
+    ["no", "NOR", "Norway"],
+    ["om", "OMN", "Oman"],
+    ["pk", "PAK", "Pakistan"],
+    ["pw", "PLW", "Palau"],
+    ["ps", "PSE", "Palestine, State of"],
+    ["pa", "PAN", "Panama"],
+    ["pg", "PNG", "Papua New Guinea"],
+    ["py", "PRY", "Paraguay"],
+    ["pe", "PER", "Peru"],
+    ["ph", "PHL", "Philippines"],
+    ["pn", "PCN", "Pitcairn"],
+    ["pl", "POL", "Poland"],
+    ["pt", "PRT", "Portugal"],
+    ["pr", "PRI", "Puerto Rico"],
+    ["qa", "QAT", "Qatar"],
+    ["re", "REU", "Réunion"],
+    ["ro", "ROU", "Romania"],
+    ["ru", "RUS", "Russian Federation"],
+    ["rw", "RWA", "Rwanda"],
+    ["bl", "BLM", "Saint Barthélemy"],
+    ["sh", "SHN", "Saint Helena, Ascension and Tristan da Cunha"],
+    ["kn", "KNA", "Saint Kitts and Nevis"],
+    ["lc", "LCA", "Saint Lucia"],
+    ["mf", "MAF", "Saint Martin (French part)"],
+    ["pm", "SPM", "Saint Pierre and Miquelon"],
+    ["vc", "VCT", "Saint Vincent and the Grenadines"],
+    ["ws", "WSM", "Samoa"],
+    ["sm", "SMR", "San Marino"],
+    ["st", "STP", "Sao Tome and Principe"],
+    ["sa", "SAU", "Saudi Arabia"],
+    ["sn", "SEN", "Senegal"],
+    ["rs", "SRB", "Serbia"],
+    ["sc", "SYC", "Seychelles"],
+    ["sl", "SLE", "Sierra Leone"],
+    ["sg", "SGP", "Singapore"],
+    ["sx", "SXM", "Sint Maarten (Dutch part)"],
+    ["sk", "SVK", "Slovakia"],
+    ["si", "SVN", "Slovenia"],
+    ["sb", "SLB", "Solomon Islands"],
+    ["so", "SOM", "Somalia"],
+    ["za", "ZAF", "South Africa"],
+    ["gs", "SGS", "South Georgia and the South Sandwich Islands"],
+    ["ss", "SSD", "South Sudan"],
+    ["es", "ESP", "Spain"],
+    ["lk", "LKA", "Sri Lanka"],
+    ["sd", "SDN", "Sudan"],
+    ["sr", "SUR", "Suriname"],
+    ["sj", "SJM", "Svalbard and Jan Mayen"],
+    ["sz", "SWZ", "Swaziland"],
+    ["se", "SWE", "Sweden"],
+    ["ch", "CHE", "Switzerland"],
+    ["sy", "SYR", "Syrian Arab Republic"],
+    ["tw", "TWN", "Taiwan, Province of China"],
+    ["tj", "TJK", "Tajikistan"],
+    ["tz", "TZA", "Tanzania, United Republic of"],
+    ["th", "THA", "Thailand"],
+    ["tl", "TLS", "Timor-Leste"],
+    ["tg", "TGO", "Togo"],
+    ["tk", "TKL", "Tokelau"],
+    ["to", "TON", "Tonga"],
+    ["tt", "TTO", "Trinidad and Tobago"],
+    ["tn", "TUN", "Tunisia"],
+    ["tr", "TUR", "Turkey"],
+    ["tm", "TKM", "Turkmenistan"],
+    ["tc", "TCA", "Turks and Caicos Islands"],
+    ["tv", "TUV", "Tuvalu"],
+    ["ug", "UGA", "Uganda"],
+    ["ua", "UKR", "Ukraine"],
+    ["ae", "ARE", "United Arab Emirates"],
+    ["gb", "GBR", "United Kingdom"],
+    ["us", "USA", "United States"],
+    ["um", "UMI", "United States Minor Outlying Islands"],
+    ["uy", "URY", "Uruguay"],
+    ["uz", "UZB", "Uzbekistan"],
+    ["vu", "VUT", "Vanuatu"],
+    ["ve", "VEN", "Venezuela, Bolivarian Republic of"],
+    ["vn", "VNM", "Viet Nam"],
+    ["vg", "VGB", "Virgin Islands, British"],
+    ["vi", "VIR", "Virgin Islands, U.S."],
+    ["wf", "WLF", "Wallis and Futuna"],
+    ["eh", "ESH", "Western Sahara"],
+    ["ye", "YEM", "Yemen"],
+    ["zm", "ZMB", "Zambia"],
+    ["zw", "ZWE", "Zimbabwe"] ];
+
+    console.log(country_codes[9][0]);
